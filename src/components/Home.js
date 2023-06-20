54631185c77cf3d38d173f884a73d287c48e8040
juancso <juancso69@gmail.com>
2023-06-20 11:18:07 -0300

    subindo projeto

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+function Home (){
+    return (
+        <div>
+            <h1>Bem vindo!!!</h1>
+            <p>Esta é a minha página principal!!!
+            </p>
+        </div>
+    )
+}
+
+export default Home;
